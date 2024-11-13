bb90f35a773b761ea84957d35a4c7961be0211d7
randyRivera0 <rariberm@espol.edu.ec>
2024-11-13 14:19:10 -0500

    App.tsx deploying problems solved

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
  // Grid version 2
  import Grid from '@mui/material/Grid2' 
 
 function App() {
-  const [count, setCount] = useState(0)
+//  const [count, setCount] = useState(0)
 
   return (
     <Grid container spacing={5}>
